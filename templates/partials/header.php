d3552830302feb76d2d26152fb10aafb646a5251
OlesZadorozhnyy <oleszadorozhnyy@gmail.com>
2016-07-06 17:45:03 +0300

    Deleted useless files

--- templates/partials/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-	<?php if(Session::exists(Config::get('session.userId'))) : ?>
-		<form method="POST" action="../user/logout">
-			<input type="submit" value="Log Out">
-		</form>
-	<?php endif; ?>
-</header>
